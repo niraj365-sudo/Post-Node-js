b0393f1cdcf66fcbb9e63a81fe53b5a1827b8ff5
Niraj Paudel <www.nirajpaudel@gmail.com>
2023-04-05 10:11:22 +0545

    Test phase

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = (passport)=> {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: 'http://connectus.cyclic.app/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done)=>{
         const newUser = {
